695943d1b014e2d669e84ed079b8fd7c3767588c
Javid <javid.momeni@gmail.com>
2024-06-07 13:52:50 +0330

    fix some dependency issues

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,14 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import tsconfigPaths from 'vite-tsconfig-paths';
+
+// export default defineConfig({
+//   plugins: [react(), tsconfigPaths()],
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
+import basicSsl from '@vitejs/plugin-basic-ssl';
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), basicSsl()],
   build: {
     rollupOptions: {
       output: {
