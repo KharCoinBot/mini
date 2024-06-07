fbd068572a748be70b2320063cc285c868f4427b
Javid <javid.momeni@gmail.com>
2024-06-07 14:20:32 +0330

    reconfig vite

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,29 +10,29 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 export default defineConfig({
   plugins: [react(), basicSsl()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: [
-            'react',
-            'react-dom',
-            // 'react-router-dom',
-            // 'react-transition-group',
-            '@twa-dev/sdk',
-            'react-microsoft-clarity',
-          ],
-          // app: ['./src/main.tsx', './src/App.tsx'],
-          // icons: ['emoji-mart', '@tabler/icons-react'],
-          framework: [
-            // '@mantine/carousel',
-            '@mantine/core',
-            '@mantine/hooks',
-            // '@mantine/modals',
-            // '@mantine/notifications',
-          ],
-        },
-      },
-    },
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks: {
+    //       vendor: [
+    //         'react',
+    //         'react-dom',
+    //         // 'react-router-dom',
+    //         // 'react-transition-group',
+    //         '@twa-dev/sdk',
+    //         'react-microsoft-clarity',
+    //       ],
+    //       // app: ['./src/main.tsx', './src/App.tsx'],
+    //       // icons: ['emoji-mart', '@tabler/icons-react'],
+    //       framework: [
+    //         // '@mantine/carousel',
+    //         '@mantine/core',
+    //         '@mantine/hooks',
+    //         // '@mantine/modals',
+    //         // '@mantine/notifications',
+    //       ],
+    //     },
+    //   },
+    // },
     outDir: './docs',
     chunkSizeWarningLimit: 600,
     // minify: 'terser',
