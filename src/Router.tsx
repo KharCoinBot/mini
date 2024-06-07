448db61f2169c632bd73d81e364f71df6775b6b1
Javid <javid.momeni@gmail.com>
2024-06-07 14:47:28 +0330

    fix routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,29 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { HomePage } from './pages/Home.page';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <HomePage />,
+//   },
+// ]);
+
+// export function Router() {
+//   return <RouterProvider router={router} />;
+// }
+
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-]);
-
-export function Router() {
-  return <RouterProvider router={router} />;
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
