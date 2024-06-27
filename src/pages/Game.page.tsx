706dd8510541b5de3e3fb8355e00c19c5a4f7c26
Javid <javid.momeni@gmail.com>
2024-06-27 09:37:36 +0330

    Feat: Game

--- src/pages/Game.page.tsx
+++ src/pages/Game.page.tsx
@@ -105,7 +105,7 @@ export function GamePage() {
                   radius="sm"
                 >              
                   <Text>
-                  خرمون
+                  خربازیامون
                   </Text>                  
                   </BackgroundImage>
               </Box>              
