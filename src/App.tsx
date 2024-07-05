a8a205c96a049c49d797b7e407ebee185828f147
Javid <javid.momeni@gmail.com>
2024-07-05 09:05:49 +0330

    Feat: start using mantin notification system

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import '@mantine/core/styles.css';
-import { AppShell, Container, DirectionProvider, MantineProvider, Title } from '@mantine/core';
+import { DirectionProvider, MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
 import React from 'react';
-import FooterMenu from './components/FooterMenu/FooterMenu';
-import HeaderMenu from './components/HeaderMenu/HeaderMenu';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { Notifications } from '@mantine/notifications';
 
 export default function App() {
-  // const user = WebApp.initDataUnsafe.user;
-  // const usertgid = user?.id;
   return (
     <React.StrictMode>
       <TonConnectUIProvider manifestUrl="https://kc.rahomaskan.com/tonconnect-manifest.json">
         <DirectionProvider initialDirection="rtl">
           <MantineProvider theme={theme} forceColorScheme="dark">
+          <Notifications />
             <Router />
           </MantineProvider>
         </DirectionProvider>
